6b0775ff50cfae05b5b4f024950f79764222e356
rohanaceres1 <ccarvalho@stone.com.br>
2017-07-13 19:11:37 -0300

    :construction: Add raw application.

---
+++ lib/btw_brainfuck.ex
@@ -0,0 +1,18 @@
+defmodule BtwBrainfuck do
+  @moduledoc """
+  Documentation for BtwBrainfuck.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> BtwBrainfuck.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/btw_brainfuck_test.exs
@@ -0,0 +1,8 @@
+defmodule BtwBrainfuckTest do
+  use ExUnit.Case
+  doctest BtwBrainfuck
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
