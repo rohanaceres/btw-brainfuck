df46432c595905d1c3d37bf0837d61781df16b0c
Ceres Rohana <ceres.rohana@gmail.com>
2017-10-24 16:38:09 -0200

    :doughnut: Add process function tests.

--- test/cli_test.exs
+++ test/cli_test.exs
@@ -4,6 +4,7 @@ defmodule CliTest do
   
     import BtwBrainfuck.CLI
 
+    # parse_arguments tests
     test ":help returned by option parsing with -h and --help options" do
         assert parse_arguments([ "-h", "some_argument" ]) == :help
         assert parse_arguments([ "--help", "some_another_argument" ]) == :help
@@ -13,5 +14,13 @@ defmodule CliTest do
     end
     test "Arguments returned by option parsing with valid options" do
         assert parse_arguments([ "--cmd", "..-.-----." ]) == { :cmd, "..-.-----."  }
+    end
+
+    # process tests
+    test ":help returned if :help sent as argument" do
+        assert process(:help) == :help
+    end
+    test ":ok returned if :help NOT sent as argument" do
+        assert process("something other than :help") == :ok
     end
   end
